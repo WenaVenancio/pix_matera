f99aa692066c44c0b0a0404d2db33a6aa9238bfb
Wena Venancio <wenadpaula97@gmail.com>
2021-09-28 21:10:22 -0300

    Criar user e alterar conta corrente

---
+++ src/main/java/matera/bootcamp/pix/domain/model/ContaCorrente.java
@@ -0,0 +1,22 @@
+package matera.bootcamp.pix.domain.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class ContaCorrente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long agencia;
+
+    @Column(nullable = false)
+    private Long conta;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal saldo;
+
+}
---
+++ src/main/java/matera/bootcamp/pix/domain/model/Usuario.java
@@ -0,0 +1,23 @@
+package matera.bootcamp.pix.domain.model;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String sobrenome;
+
+    @OneToOne(optional = false)
+    private ContaCorrente contaCorrente;
+
+}
